a3cd2a4f14cf9c488f7409b9af2308d6c8bcd852
DoomMetalLady <176007837+DoomMetalLady@users.noreply.github.com>
2024-09-22 03:42:04 -0300

    Update aplicacion.py
    vivi enriquez prueba 1 en git

--- aplicacion.py
+++ aplicacion.py
@@ -18,7 +18,11 @@ costo_servicio  = 1000
 # iva 21%
 costo_servicio_iva = costo_servicio * 0.21
 # PAIS 8%
+costo_servicio_pais = costo_servicio * 0.08
 #Ganancias 30%
+costo_servicio_ganancias = costo_servicio * 0.30
 # IIBB CABA 2%
+costo_servicio_iibb = costo_servicio * 0.02
+
 valor_final = costo_servicio + costo_servicio_iva
 print("El valor final a pagar es: ", valor_final)
